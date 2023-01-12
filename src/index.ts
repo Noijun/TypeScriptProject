e7f963e260d1fc0cdfc68fe238f5f1a55c00e8e8
jogong <shdlwns1234@naver.com>
2023-01-12 21:53:28 +0900

    fix: 2-2장 실습으로 index수정

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
-import { testMakePerson } from "./utils/makePerson";
+const MAX_AGE = 100;
+
+interface Iperson {
+  name: string;
+  age: number;
+}
+
+class Person implements Iperson {
+  constructor(public name: string, public age: number) {}
+}
+
+function makeRandomNumber(max: number = MAX_AGE): number {
+  return Math.ceil(Math.random() * max);
+}
+
+const makePerson = (name: string, age: number = makeRandomNumber()) => ({
+  name,
+  age,
+});
+
+const testMakePerson = (): void => {
+  const jane: Iperson = makePerson("Jane");
+  const jack: Iperson = makePerson("Jack");
+  console.log(jane, jack);
+};
+
 testMakePerson();
